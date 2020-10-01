2dbb92e148357325a852470c20b73c4fac741a38
uttampagda89 <72185325+uttampagda89@users.noreply.github.com>
2020-10-01 11:20:39 +0530

    Update array.py

--- array.py
+++ array.py
@@ -11,7 +11,7 @@ print()
 # creating an array with float type
 b = array('d', [2.5, 3.2, 3.3])
 
-# printing original array
+# printing original array 1
 print("The new created array is : ", end=" ")
 for i in range(0, 3):
     print(b[i], end=" ")
