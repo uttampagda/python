12c50150cb43ad34ea70aba043ca901286df0485
uttampagda89 <72185325+uttampagda89@users.noreply.github.com>
2020-10-01 11:19:01 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,17 @@
+from array import *
+
+a = array('I', [1, 2, 3])
+
+# printing original array
+print("The new created array is : ", end=" ")
+for i in range(0, 3):
+    print(a[i], end=" ")
+print()
+
+# creating an array with float type
+b = array('d', [2.5, 3.2, 3.3])
+
+# printing original array
+print("The new created array is : ", end=" ")
+for i in range(0, 3):
+    print(b[i], end=" ")
